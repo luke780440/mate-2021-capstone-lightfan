ee7de0fa86e546256df4e1d8ee744df01b8a6ba4
luke780440 <91250800+luke780440@users.noreply.github.com>
2021-11-09 16:29:42 +0800

    Update Comms.py
    Modified the packet-receiving code by using a loop. Made some other minor additions, like fixing some syntax errors. Added a basic interface for the user as well.

--- Comms.py
+++ Comms.py
@@ -1,3 +1,4 @@
+from time import sleep
 from threading import Thread
 import serial
 
@@ -11,14 +12,18 @@ class Comms:
         
     def run(self):
         while True:
-            packet = self.ser.read()
-            packets = packet.split(chr(127))
-            self.photoResistor = packets[0]
-            self.rpm = packets[1]
-            self.temp = packets[2]
+            packets = self.ser.read(64)
+            packet_list = packets.split(chr(0x7f))
+            
+            for packet in packet_list:
+                if len(packet) != 3:
+                    continue
+                self.photoResistor = packet[0]
+                self.rpm = packet[1]
+                self.temp = packet[2]
             lightVal = int(photoResistor)/255 * 1053 
             fan_speed = int(lightVal/1053 * 255)   
-            new_packet = self.ser.write(fan_speed)
+            self.ser.write(fan_speed)
 
     def start_thread(self):
         start_thread = Thread(target = self.run) #initializing the thread 
@@ -26,4 +31,23 @@ class Comms:
         
 
 ob = Comms("/dev/tty.usbmodem14201", 9600)
-ob.start_thread()
+#ob.start_thread()
+
+
+#basic interface code
+user_input = input("Would you like to turn on the fan? Enter Y for yes or N for no: ")
+if user_input == Y:
+    print("Hi hi!")
+    while True:
+        ob.start_thread()
+        print("Light values:", photoResistor)
+        sleep(2)
+        print("Fan speed:", fan_speed)
+        sleep(2)
+
+else:
+    print("Bye bye")
+    
+
+
+
